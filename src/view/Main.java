d01f18772ed70c2c203774abe966c40018654b5b
uiuqM <wilque.coelho@mail.uft.edu.br>
2022-10-25 01:05:51 -0300

    Pacote model

---
+++ src/view/Main.java
@@ -0,0 +1,11 @@
+package view;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = "wilque";
+		System.out.println(nome);
+	}
+
+}
